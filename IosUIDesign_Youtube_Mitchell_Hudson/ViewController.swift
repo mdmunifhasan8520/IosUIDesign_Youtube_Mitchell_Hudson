027429e17fb5a79ee6cb0d4110bb8aed1416bd2b
pulok <mdmunifhasan8520@gmail.com>
2018-11-14 03:23:20 +0600

    complete inset bounds >>>>output second image

--- IosUIDesign_Youtube_Mitchell_Hudson/ViewController.swift
+++ IosUIDesign_Youtube_Mitchell_Hudson/ViewController.swift
@@ -14,20 +14,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let box = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 250))
+        let box = UIView(frame: CGRect(x: 50, y: 0, width: 300, height: 250))
         box.backgroundColor = UIColor.cyan
         view.addSubview(box)
         
-        box.center = CGPoint(x: 100, y: 500)
+        //box.center = CGPoint(x: 100, y: 500)
         
-        let smallbox = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        //box.center.x = view.bounds.width / 6
+        box.center.y = view.bounds.height / 3
+        
+        //let r = CGRect(x: 10, y: 10, width: 280, height: 230)
+        let r = box.bounds.insetBy(dx: 10, dy: 10)
+        let smallbox = UIView(frame: r)
         smallbox.backgroundColor = UIColor.black
         box.addSubview(smallbox)
-        
+        /*
         smallbox.center.x = box.bounds.width / 2
         smallbox.center.y = box.bounds.height / 2
         
-        
+        */
         
      
     }
